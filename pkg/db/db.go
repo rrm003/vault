8e8b6f1ecb902dfea3814007fb57e8427a09a946
rrm003 <rammankar003@gmail.com>
2023-11-15 12:06:11 -0600

    refactored db connection

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -16,20 +17,35 @@ func StartDB() (*pg.DB, error) {
 
 	//check if we are in prod
 	//then use the db url from the env
-	if os.Getenv("ENV") == "PROD" {
-		opts, err = pg.ParseURL(os.Getenv("DATABASE_URL"))
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		opts = &pg.Options{
-			//default port
-			//depends on the db service from docker compose
-			Addr:     "db:5432",
-			User:     "postgres",
-			Password: "admin",
-		}
-	}
+	// if os.Getenv("ENV") == "PROD" {
+	// 	opts, err = pg.ParseURL(os.Getenv("DATABASE_URL"))
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// } else {
+	// 	opts = &pg.Options{
+	// 		//default port
+	// 		//depends on the db service from docker compose
+	// 		Addr:     "db:5432",
+	// 		User:     "postgres",
+	// 		Password: "admin",
+	// 	}
+	// }
+
+	// Read database connection details from environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Construct the connection options
+	opts = &pg.Options{
+		Addr:     fmt.Sprintf("%s:%s", dbHost, dbPort),
+		User:     dbUser,
+		Password: dbPassword,
+		Database: dbName,
+	} //"postgres://postgres:loop@007@34.122.49.254:5432/vault-db")
 
 	//connect db
 	db := pg.Connect(opts)
