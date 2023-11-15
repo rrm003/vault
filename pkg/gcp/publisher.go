9dc6b0715ba0950bf8074bacc24aaa74116d16be
rrm003 <rammankar003@gmail.com>
2023-11-15 12:07:18 -0600

    added support for pub sub

---
+++ pkg/gcp/publisher.go
@@ -0,0 +1,52 @@
+package gcp
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"cloud.google.com/go/pubsub"
+	"google.golang.org/api/option"
+)
+
+type Event struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Msg   string `json:"msg"`
+}
+
+func GetPubSvc(ctx context.Context) (*pubsub.Client, error) {
+	fmt.Println("publishing msg for ", projectID)
+
+	client, err := pubsub.NewClient(ctx, "valut-svc", option.WithCredentialsFile("./valut-svc-firebase-adminsdk4.json"))
+	if err != nil {
+		fmt.Printf("Error creating Pub/Sub client: %v\n", err)
+		return nil, err
+	}
+
+	defer client.Close()
+
+	return client, nil
+}
+
+func PublishMessage(ctx context.Context, t *pubsub.Topic, e *Event) error {
+	fmt.Printf("received event %+v\n", e)
+
+	msg, err := json.Marshal(e)
+	if err != nil {
+		fmt.Println("failed to marshal event", err)
+		return err
+	}
+
+	result := t.Publish(ctx, &pubsub.Message{
+		Data: []byte(msg),
+	})
+
+	id, err := result.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("pubsub: result.Get: %w", err)
+	}
+
+	fmt.Printf("Published a message; msg ID: %v\n", id)
+	return nil
+}
--- pkg/web/app.go
+++ pkg/web/app.go
@@ -1,14 +1,20 @@
 package web
 
 import (
+	"context"
 	"log"
 
+	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/storage"
 	"firebase.google.com/go/v4/auth"
 	"github.com/go-pg/pg/v10"
 )
 
 type AppSvc struct {
+	Ctx        context.Context
 	DB         *pg.DB
 	Logger     *log.Logger
 	AuthClient *auth.Client
+	StorageSvc *storage.BucketHandle
+	TopicOTP   *pubsub.Topic
 }
