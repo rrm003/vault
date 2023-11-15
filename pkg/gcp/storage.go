0ff220c09bb36150983ce0fe312e817f6d4edda3
rrm003 <rammankar003@gmail.com>
2023-11-15 12:09:03 -0600

    integrate with cloud storage

--- pkg/gcp/storage.go
+++ pkg/gcp/storage.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"os"
@@ -17,12 +16,12 @@ import (
 
 var (
 	bucketName  = os.Getenv("BUCKET_NAME")
-	projectID   = os.Getenv("PROJECT_ID")
+	projectID   = "valut-svc" //os.Getenv("PROJECT_ID")
 	keyFilePath = os.Getenv("KEY_FILE_PATH")
 )
 
 type FileList struct {
-	Name         []string  `json:"name"`
+	Name         string    `json:"name"`
 	Type         string    `json:"type"`
 	ModifiedTime time.Time `json:"modified_time"`
 	Size         int64     `json:"size"`
@@ -32,7 +31,7 @@ func GetStorageSvc() (*storage.BucketHandle, error) {
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
+		log.Printf("Failed to create client: %v", err)
 
 		return nil, err
 	}
@@ -43,9 +42,9 @@ func GetStorageSvc() (*storage.BucketHandle, error) {
 	return bucket, nil
 }
 
-func ListObjects(bucket *storage.BucketHandle) ([]*FileList, error) {
-	fmt.Println("Objects in the bucket:")
-	query := &storage.Query{Prefix: "rrm" + "/", Delimiter: ""}
+func ListObjects(bucket *storage.BucketHandle, folderPath string) ([]*FileList, error) {
+	fmt.Println("Objects in the bucket:", folderPath)
+	query := &storage.Query{Prefix: folderPath + "/", Delimiter: "/"}
 
 	var names []*FileList
 	it := bucket.Objects(context.Background(), query)
@@ -56,17 +55,23 @@ func ListObjects(bucket *storage.BucketHandle) ([]*FileList, error) {
 		}
 
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
 		}
 
 		fmt.Printf("file attributes : %+v\n", attrs.Name)
-		if attrs.Name[len(attrs.Name)-1] != '/' {
+
+		atrrLen := len(attrs.Name)
+		if atrrLen > 0 && attrs.Name[atrrLen-1] != '/' {
 			path := strings.Split(attrs.Name, "/")
-			names = append(names, &FileList{Name: path, Type: "file", ModifiedTime: attrs.Updated, Size: attrs.Size})
+			names = append(names, &FileList{Name: path[len(path)-1], Type: "file", ModifiedTime: attrs.Updated, Size: attrs.Size})
 		} else {
-			path := strings.Split(attrs.Name, "/")
-			names = append(names, &FileList{Name: path, Type: "folder", ModifiedTime: attrs.Updated, Size: attrs.Size})
+			prefix := strings.Split(attrs.Prefix, "/")
+			fmt.Println(prefix, "len", len(prefix))
+			if len(prefix) >= 2 {
+				names = append(names, &FileList{Name: prefix[len(prefix)-2], Type: "folder", ModifiedTime: attrs.Updated, Size: attrs.Size})
+			}
 		}
+
 	}
 
 	return names, nil
@@ -87,16 +92,24 @@ func CreateFile(bucket *storage.BucketHandle, path, objectName string, file mult
 	return nil
 }
 
-func CreateFolder(bucket *storage.BucketHandle, folderName string) {
-	obj := bucket.Object(folderName)
+func CreateFolder(bucket *storage.BucketHandle, folderName string) error {
+	fmt.Println("creating foldername", folderName)
+
+	obj := bucket.Object(folderName + "/")
+
 	w := obj.NewWriter(context.Background())
 	if _, err := w.Write([]byte("")); err != nil {
-		log.Fatalf("Failed to create folder: %v", err)
+		log.Printf("Failed to create folder: %v\n", err)
+		return err
 	}
+
 	if err := w.Close(); err != nil {
-		log.Fatalf("Failed to close folder: %v", err)
+		log.Printf("Failed to close folder: %v\n", err)
+		return err
 	}
+
 	fmt.Printf("Folder %s created successfully.\n", folderName)
+	return nil
 }
 
 func UpdateFile(bucket *storage.BucketHandle, objectName string, newContent []byte) {
@@ -104,23 +117,28 @@ func UpdateFile(bucket *storage.BucketHandle, objectName string, newContent []by
 	w := obj.NewWriter(context.Background())
 	_, err := w.Write(newContent)
 	if err != nil {
-		log.Fatalf("Failed to update file: %v", err)
+		log.Printf("Failed to update file: %v", err)
 	}
 	if err := w.Close(); err != nil {
-		log.Fatalf("Failed to close updated file: %v", err)
+		log.Printf("Failed to close updated file: %v", err)
 	}
 	fmt.Printf("File %s updated successfully.\n", objectName)
 }
 
-func DeleteFile(bucket *storage.BucketHandle, objectName string) {
+func DeleteFile(bucket *storage.BucketHandle, objectName string) error {
 	obj := bucket.Object(objectName)
 	if err := obj.Delete(context.Background()); err != nil {
-		log.Fatalf("Failed to delete file: %v", err)
+		log.Printf("Failed to delete file: %v", err)
+		return err
 	}
+
 	fmt.Printf("File %s deleted successfully.\n", objectName)
+	return nil
 }
 
-func DeleteFolder(bucket *storage.BucketHandle, folderName string) {
+func DeleteFolder(bucket *storage.BucketHandle, folderName string) error {
+	fmt.Println("deleting folder:", folderName)
+
 	query := &storage.Query{Prefix: folderName}
 
 	it := bucket.Objects(context.Background(), query)
@@ -130,31 +148,55 @@ func DeleteFolder(bucket *storage.BucketHandle, folderName string) {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			return err
 		}
+
+		fmt.Println("files in folder ", attrs.Name)
 		if err := bucket.Object(attrs.Name).Delete(context.Background()); err != nil {
-			log.Fatalf("Failed to delete object in the folder: %v", err)
+			log.Println("Failed to delete object in the folder: %v", err)
+			return err
 		}
 	}
 
 	// Now, delete the folder itself
-	obj := bucket.Object(folderName)
-	if err := obj.Delete(context.Background()); err != nil {
-		log.Fatalf("Failed to delete folder: %v", err)
-	}
+	// obj := bucket.Object(folderName)
+	// if err := obj.Delete(context.Background()); err != nil {
+	// 	log.Println("Failed to delete folder: %v", err)
+	// 	return err
+	// }
 	fmt.Printf("Folder %s and its contents deleted successfully.\n", folderName)
+	return nil
 }
 
-func FetchFile(bucket *storage.BucketHandle, objectName string) {
-	obj := bucket.Object(objectName)
-	r, err := obj.NewReader(context.Background())
-	if err != nil {
-		log.Fatalf("Failed to fetch file: %v", err)
-	}
-	defer r.Close()
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		log.Fatalf("Failed to read file content: %v", err)
-	}
-	fmt.Printf("Fetched content from %s:\n%s\n", objectName, string(content))
+func FetchFile(bucket *storage.BucketHandle, objectName string) (string, error) {
+
+	// accessID := "firebase-adminsdk-8r5g6@valut-svc.iam.gserviceaccount.com"
+	// opts := &storage.SignedURLOptions{
+	// 	Scheme:         storage.SigningSchemeV4,
+	// 	GoogleAccessID: accessID,
+	// 	Method:         "GET",                           // The HTTP method for the signed URL
+	// 	Expires:        time.Now().Add(5 * time.Minute), // The expiration time for the URL
+	// }
+
+	// url, err := bucket.SignedURL(objectName, opts)
+	// if err != nil {
+	// 	log.Printf("failed to get signed url: %v", err)
+	// 	return "", err
+	// }
+	// fmt.Println("signed url ", url)
+
+	// obj := bucket.Object(objectName)
+	// r, err := obj.NewReader(context.Background())
+	// if err != nil {
+	// 	log.Println("Failed to fetch file: %v", err)
+	// }
+	// defer r.Close()
+	// content, err := ioutil.ReadAll(r)
+	// if err != nil {
+	// 	log.Println("Failed to read file content: %v", err)
+	// }
+	// fmt.Printf("Fetched content from %s:\n%s\n", objectName, string(content))
+
+	return "", nil
 }
--- pkg/web/file.go
+++ pkg/web/file.go
@@ -1,32 +1,80 @@
 package web
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/rrm003/valut/pkg/gcp"
 )
 
+type FetchFileReq struct {
+	Path string `json:"path"`
+	OTP  string `json:"otp"`
+}
+
+type FetchFileResp struct {
+	URL string `json:"url"`
+}
+
+type DeleteFileReq struct {
+	Path string `json:"path"`
+	Type string `json:"type"`
+}
+
+type CreateFolderReq struct {
+	Path string `json:"path"`
+}
+
+type UserFileOTP struct {
+	ID        uuid.UUID `json:"id"`
+	UserID    uuid.UUID `json:"user_id"`
+	Path      string    `json:"file_path"`
+	OTP       string    `json:"otp"`
+	CreatedAt time.Time `json:"created_at"`
+	ExpiryAt  time.Time `json:"expires_at"`
+}
+
+// Define the character set for the alphanumeric code
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
 func (app *AppSvc) FileCreateHandler(w http.ResponseWriter, r *http.Request) {
+	userUID, ok := r.Context().Value("userid").(string)
+	if !ok {
+		// Handle the case where userUID is not available in the context
+		http.Error(w, "User ID not found", http.StatusUnauthorized)
+		return
+	}
+
+	userUID = fmt.Sprintf("%s/", userUID)
 	file, handler, err := r.FormFile("file")
-	// fileName := r.FormValue("file_name")
 	if err != nil {
 		fmt.Println("file create", "failed to read file", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
+	path := r.FormValue("path")
+	fmt.Println("uploading file at", path)
+
 	defer file.Close()
 
-	storageSvc, err := gcp.GetStorageSvc()
-	if err != nil {
-		fmt.Println("file create", "failed to get storage svc", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+	if path == "" {
+		path = userUID
+	} else {
+		path = userUID + path + "/"
 	}
 
-	err = gcp.CreateFile(storageSvc, "rrm/", handler.Filename, file)
+	err = gcp.CreateFile(app.StorageSvc, path, handler.Filename, file)
 	if err != nil {
 		fmt.Println("file create", "failed to store file in bucket", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -37,6 +85,34 @@ func (app *AppSvc) FileCreateHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *AppSvc) FileListHandler(w http.ResponseWriter, r *http.Request) {
+	userUID, ok := r.Context().Value("userid").(string)
+	if !ok {
+		// Handle the case where userUID is not available in the context
+		http.Error(w, "User ID not found", http.StatusUnauthorized)
+		return
+	}
+
+	userUID = fmt.Sprintf("%s", userUID)
+	fmt.Println("request headers", r.Header)
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("error reading request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	fmt.Println("received req body ", string(reqBody))
+	req := &FetchFileReq{}
+	err = json.Unmarshal(reqBody, req)
+	if err != nil {
+		fmt.Println("error unmarshalling request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("request body : %+v", req)
+
 	storageSvc, err := gcp.GetStorageSvc()
 	if err != nil {
 		fmt.Println("file create", "failed to get storage svc", err)
@@ -44,11 +120,21 @@ func (app *AppSvc) FileListHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	list, err := gcp.ListObjects(storageSvc)
+	if req.Path == "" {
+		req.Path = userUID
+	} else {
+		req.Path = userUID + "/" + req.Path
+	}
+
+	list, err := gcp.ListObjects(storageSvc, req.Path)
 	if err != nil {
 		fmt.Println("file create", "failed to store file in bucket", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
+	}
+
+	if list == nil {
+		list = []*gcp.FileList{}
 	}
 
 	resp, err := json.Marshal(list)
@@ -61,4 +147,258 @@ func (app *AppSvc) FileListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	io.WriteString(w, string(resp))
+}
+
+// Function to generate a random alphanumeric code of a given length
+func generateRandomCode() string {
+	rand.Seed(time.Now().UnixNano())
+	code := make([]byte, 6)
+	for i := range code {
+		code[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(code)
+}
+
+func (app *AppSvc) FetchFileOTPHandler(w http.ResponseWriter, r *http.Request) {
+	userUID, ok := r.Context().Value("userid").(string)
+	if !ok {
+		// Handle the case where userUID is not available in the context
+		http.Error(w, "User ID not found", http.StatusUnauthorized)
+		return
+	}
+	// userUID := "8bd67aed-6ec5-48a4-a6b6-73e794ca12d6"
+	id := userUID
+
+	fmt.Println("fetch file request", r)
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("error reading request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	body := &FetchFileReq{}
+	err = json.Unmarshal(reqBody, body)
+	if err != nil {
+		fmt.Println("error unmarshalling request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("req body %+v\n", body)
+
+	body.Path = fmt.Sprintf("%s/%s", userUID, body.Path)
+
+	opt := generateRandomCode()
+	// create user file otp combination
+	_, err = app.DB.Exec("insert into user_file_otp(user_id, file_path, otp, expires_at)  values(?, ?, ?, ?)", id, body.Path, opt, time.Now().Add(35*time.Second))
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Printf("no record found for combination of user = %s file %s otp %s \n", userUID, body.Path, body.OTP)
+			w.WriteHeader(http.StatusUnauthorized)
+
+			return
+		}
+
+		fmt.Println("error reading db", err)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	user := &User{}
+	fmt.Println("DB instance : ", app.DB)
+	_, err = app.DB.Query(user, "select * from users where id = ?", userUID)
+	if err != nil {
+		fmt.Println("error reading db", err)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	fmt.Println("user :", user)
+
+	e := &gcp.Event{
+		Name:  "File access OTP",
+		Email: user.PrimaryEmail,
+		Msg:   fmt.Sprintf("OTP %s for file %s", opt, body.Path),
+	}
+
+	// send otp to email of user
+	err = gcp.PublishMessage(app.Ctx, app.TopicOTP, e)
+	if err != nil {
+		fmt.Println("failed to publish msg", err)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (app *AppSvc) FetchFileHandler(w http.ResponseWriter, r *http.Request) {
+	userUID, ok := r.Context().Value("userid").(string)
+	if !ok {
+		// Handle the case where userUID is not available in the context
+		http.Error(w, "User ID not found", http.StatusUnauthorized)
+		return
+	}
+	// userUID := "8bd67aed-6ec5-48a4-a6b6-73e794ca12d6"
+
+	userUID = fmt.Sprintf("%s/", userUID)
+
+	fmt.Println("fetch file request", r)
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("error reading request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	body := &FetchFileReq{}
+	err = json.Unmarshal(reqBody, body)
+	if err != nil {
+		fmt.Println("error unmarshalling request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("req body %+v\n", body)
+
+	body.Path = userUID + body.Path
+
+	// validate user file otp combination
+	ufotp := &UserFileOTP{}
+	_, err = app.DB.Query(ufotp, "select * from user_file_otp where user_id = ? and file_path = ? and otp = ? and expires_at>=now()", userUID, body.Path, body.OTP)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Printf("no record found for combination of user = %s file %s otp %s \n", userUID, body.Path, body.OTP)
+			w.WriteHeader(http.StatusUnauthorized)
+
+			return
+		}
+
+		fmt.Println("error reading db", err)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	// path, err := gcp.FetchFile(app.StorageSvc, body.Path)
+	// if err != nil {
+	// 	fmt.Println("error fetching file", err)
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	return
+	// }
+
+	fmt.Println(body.Path)
+	_, err = app.StorageSvc.Object(body.Path).Attrs(context.Background())
+	if err != nil {
+		log.Printf("Failed to get object attributes: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	resp := FetchFileResp{
+		URL: "https://storage.googleapis.com/valut-bucket/" + body.Path,
+	}
+
+	rawResp, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Println("error marshalling resp", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(rawResp)
+}
+
+func (app *AppSvc) FileDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	userUID, ok := r.Context().Value("userid").(string)
+	if !ok {
+		// Handle the case where userUID is not available in the context
+		http.Error(w, "User ID not found", http.StatusUnauthorized)
+		return
+	}
+
+	userUID = fmt.Sprintf("%s/", userUID)
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("error reading request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	req := &DeleteFileReq{}
+	err = json.Unmarshal(reqBody, req)
+	if err != nil {
+		fmt.Println("error unmarshalling request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	req.Path = userUID + req.Path
+	switch req.Type {
+	case "file":
+		err = gcp.DeleteFile(app.StorageSvc, req.Path)
+		if err != nil {
+			fmt.Println("failed to delete file :", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+	case "folder":
+		err = gcp.DeleteFolder(app.StorageSvc, req.Path)
+		if err != nil {
+			fmt.Println("failed to delete folder :", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+	default:
+		fmt.Println("unknown file type", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+func (app *AppSvc) CreateFolderHandler(w http.ResponseWriter, r *http.Request) {
+	userUID, ok := r.Context().Value("userid").(string)
+	if !ok {
+		// Handle the case where userUID is not available in the context
+		http.Error(w, "User ID not found", http.StatusUnauthorized)
+		return
+	}
+
+	userUID = fmt.Sprintf("%s/", userUID)
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("error reading request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	req := &CreateFolderReq{}
+	err = json.Unmarshal(reqBody, req)
+	if err != nil {
+		fmt.Println("error unmarshalling request body", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = gcp.CreateFolder(app.StorageSvc, userUID+req.Path)
+	if err != nil {
+		fmt.Println("error creating folder body", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("folder created")
 }
