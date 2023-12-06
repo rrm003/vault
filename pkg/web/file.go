b25a38ba59495d57c0c4c0ab4433401e03de71e2
rrm003 <rammankar003@gmail.com>
2023-12-05 18:03:42 -0600

    updated fetch file response

--- pkg/web/file.go
+++ pkg/web/file.go
@@ -361,6 +361,7 @@ func (app *AppSvc) FetchFileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(rawResp)
 }
